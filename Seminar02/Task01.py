8879b2ca1ab466e382968866aad5539a9cbfc4b3
Saqwix <iva2012.lol@gmail.com>
2022-12-23 11:23:40 +0300

    add and completed Task01Sem02

--- Seminar02/Task01.py
+++ Seminar02/Task01.py
@@ -5,3 +5,11 @@
 # - 6782 -> 23
 # - 0.67 -> 13
 # - 198.45 -> 27
+
+number = input ('Введите число: ')
+sum = 0
+for x in number:
+    if x.isdigit():
+        sum += int(x)
+
+print(f"Cyмма {number} равна: ", sum)
