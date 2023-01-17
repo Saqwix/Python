ea65c7ce5294cdee106aedef0fa183608255e990
Saqwix <iva2012.lol@gmail.com>
2023-01-17 13:50:50 +0300

    add and completed task01 seminar05

--- Seminar05/Task01.py
+++ Seminar05/Task01.py
@@ -1,4 +1,3 @@
-# Урок 5. Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension
 # 1. Напишите программу, удаляющую из текста все слова, содержащие "абв". В тексте используется разделитель пробел.
 # in
 # Number of words: 10
@@ -15,3 +14,25 @@
 # out
 # The data is incorrect
 
+# text = input("Введите текст через пробел:\n")
+# print(f"Исходный текст: {text}")
+# find_text = "абв"
+# list = [i for i in text.split() if find_text not in i]
+# print(f'Результат: {" ".join(list)}')
+
+import random
+
+text = "абв"
+print("Слово которое нужно удалить из текста: ", text)
+num_word = (int(input("Number of words: ")))
+list_word = []
+print("Random text: ")
+for x in range(num_word):
+    random_text = random.sample(text, 3)
+    list_word.append("".join(random_text))
+
+print(" ".join(list_word))
+
+print("Result: ")
+list_word2 = list(filter(lambda x: text not in x, list_word))
+print(" ".join(list_word2))
